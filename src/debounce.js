d86ee3c8b683840a90454fd93b26fb6264dbc28b
sasa <evanmao@tencent.com>
2019-09-16 23:34:33 +0800

    Re:

--- src/debounce.js
+++ src/debounce.js
@@ -1,39 +1,44 @@
 /**
- *
- * @param fn
- * @param delay
- * @param i
- * @returns {function(): Promise<any>}
+ * @function debounce
+ * @param fn {function} the function you want to limit execute
+ * @param delay {number} delay time ,default 1000ms
+ * @param immediateFlag {boolean} default false. if true, fn will execute  immediately and ignore the call within delay time;
+ * @param leaveFlag {boolean} default false. if true, fn will execute  immediately and ignore the call within delay time;
+ * @param promiseFlag {boolean}
+ * @returns {debounce}
  */
-export default function (fn, delay = 1000, i) {
-    let timer, t=0, p, pres;
+export default function debounce(fn, delay = 1000, immediateFlag = false, leaveFlag = false, promiseFlag = true) {
+    let timer, t = 0, p, pres, prej, args;
 
-    function _(ctx,args) {
-        timer = undefined;
-        p     = undefined;
-        i && (t = Date.now());
-        return fn.apply(ctx, args);
+    function _timer_(ctx, res, rej) {
+        prej  = rej;
+        pres  = res;
+        timer = setTimeout(() => {
+            if (!timer) return;
+            timer = undefined;
+            pres  = prej = p = undefined;
+            promiseFlag ? res(fn.apply(ctx, args)) : fn.apply(ctx, args);
+            t = Date.now();
+        }, delay);
     }
 
-    let retFn   = i ?
-        function () {
-            if (Date.now() - t > delay) {
-                p = Promise.resolve(_(this, arguments));
-            }
+    function retFn() {
+        if (!timer && Date.now() - t > 0 && immediateFlag) {
+            t = Date.now();
+            p = promiseFlag ? Promise.resolve(fn.apply(ctx, args)) : fn.apply(ctx, args);
             return p;
-        } : function () {
-            timer&&clearTimeout(timer);
-            let args = arguments;
-            if (p) {
-                timer = setTimeout(() => pres(_(this, args)), delay);
-            } else {
-                p = new Promise( (res)=>{
-                    pres  = res;
-                    timer = setTimeout(() => pres(_(this, args)), delay);
-                });
-            }
-            return p;
-        };
-    retFn.clear = () => timer = clearTimeout(timer);
+        }
+        if (!timer || !leaveFlag) args = arguments;
+        !p && promiseFlag ? p = new Promise((res, rej) => _timer_(this, res, rej))
+            : _timer_(this, pres, prej);
+        return p;
+    }
+
+    retFn.clear = () => {
+        timer = timer && clearTimeout(timer);
+        p     = undefined;
+        prej && prej();
+        return retFn;
+    };
     return retFn;
 }
--- src/throttle.js
+++ src/throttle.js
@@ -1,50 +1,44 @@
 /**
- * @function debounce
+ * @function throttle
  * @param fn {function} the function you want to limit execute
  * @param delay {number} delay time ,default 1000ms
  * @param immediateFlag {boolean} default false. if true, fn will execute  immediately;
  * @param leaveFlag {boolean};
  * @param promiseFlag {boolean}
- * @returns {debounce} default false. if true, fn will execute  immediately
+ * @returns {throttle} default false. if true, fn will execute  immediately
  */
-export default function debounce(fn, delay = 1000, immediateFlag=false, leaveFlag=false,promiseFlag = false) {
-    let timer, args, p,pres,prej, t = 0;
+export default function throttle(fn, delay = 1000, immediateFlag = false, leaveFlag = false, promiseFlag = false) {
+    let timer, args, p, pres, prej, t = 0;
 
-    function _(ctx, args) {
-        timer = undefined;
-        pres  = prej = p = undefined;
-        return fn.apply(ctx, args)
-    }
-
-    function _timer_(res, ctx) {
+    function _timer_(ctx, res, rej) {
+        prej  = rej;
+        pres  = res;
         timer = setTimeout(() => {
-            if (timer) {
-                promiseFlag ? res(_(ctx, args)) : _(ctx, args);
-            }
+            if (!timer) return;
+            timer = undefined;
+            pres  = prej = p = undefined;
+            promiseFlag ? res(fn.apply(ctx, args)) : fn.apply(ctx, args);
             t = Date.now();
         }, delay);
     }
 
     let retFn = immediateFlag ?
         function () {
-            if (leaveFlag && timer) return p;
-            args = arguments;
-            if (Date.now() - t > delay && !p) {
-                p = promiseFlag ? Promise.resolve(_(this, args)) : _(this, args);
+            let flag = Date.now() - t < delay;
+            if (leaveFlag && flag) return p;
+            if (flag && !p) {
+                p = promiseFlag ? Promise.resolve(fn.apply(ctx, arguments)) : fn.apply(ctx, arguments);
                 t = Date.now();
             } else if (!timer) {
-                let ths = this;
-                timer   = true;
-                p       = promiseFlag ? new Promise((res, rej) => _timer_(res, ths)) : _timer_(res, ths);
+                args = arguments;
+                promiseFlag ? p || (p = new Promise((res, rej) => _timer_(this, res, rej))) : _timer_(this);
             }
             return p;
         } : function () {
+            if (timer && leaveFlag) return p;
+            args = arguments;
             if (timer) return p;
-            args  = arguments;
-            timer = true;
-            promiseFlag ? p = new Promise((res) => {
-                timer = setTimeout(() => timer && res(_(this, args)), delay);
-            }) : setTimeout(() => timer && _(this, args), delay);
+            p = promiseFlag ? new Promise((res, rej) => _timer_(this, res, rej)) : _timer_(this);
             return p;
         };
 
@@ -52,28 +46,14 @@ export default function debounce(fn, delay = 1000, immediateFlag=false, leaveFla
      * 取消执行
      */
     retFn.clear = () => {
-        timer = clearTimeout(timer) || (timer = undefined);
-        p     = undefined;
-        prej && prej();
+        clearTimeout(timer) || (timer = undefined);
+        pres = p = undefined;
+        t    = Date.now();
+        if (prej) {
+            prej('$rej$');
+            prej = undefined;
+        }
         return retFn;
     };
-    // /**
-    //  *
-    //  * @param arg
-    //  * @return {function(): Promise<any>}
-    //  */
-    // retFn.delay = (arg) => {
-    //     delay = arg;
-    //     return retFn;
-    // };
-    // /**
-    //  *
-    //  * @param arg
-    //  * @return {function(): Promise<any>}
-    //  */
-    // retFn.promise = (arg) => {
-    //     promiseFlag = !!arg;
-    //     return retFn;
-    // };
     return retFn;
 }
